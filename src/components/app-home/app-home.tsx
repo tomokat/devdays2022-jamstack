462c519cb66ef995a22bb5deabfaa10c120c334c
Tomo Katayama <tomoyuki_katayama@otpp.com>
2022-10-11 15:19:48 -0400

    add gokul button

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -17,6 +17,9 @@ export class AppHome {
         <stencil-route-link url="/profile/stencil">
           <button>Profile page</button>
         </stencil-route-link>
+        <stencil-route-link url="/profile/gokul">
+          <button>Gokul</button>
+        </stencil-route-link>
       </div>
     );
   }
